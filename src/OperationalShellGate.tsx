4606252b6c7ab15c50a54237826c9b43c1931ca7
carlosjmunoz78 <130542695+carlosjmunoz78@users.noreply.github.com>
2026-08-22 12:39:38 +0200

    fix(preprod): isolate expediente create route

--- src/OperationalShellGate.tsx
+++ src/OperationalShellGate.tsx
@@ -1,7 +1,7 @@
 import {useLocation} from 'react-router-dom';
 import OperationalShellV2 from './OperationalShellV2';
 
-const dedicatedExact=new Set(['/contactos','/inmobiliarias','/tasaciones','/agenda','/firmas','/documentacion','/financieros','/visitadores','/informes','/buscar','/bancos','/bancos/contactos','/economia','/notificaciones','/notarias','/visitas']);
+const dedicatedExact=new Set(['/contactos','/inmobiliarias','/tasaciones','/agenda','/firmas','/documentacion','/financieros','/visitadores','/informes','/buscar','/bancos','/bancos/contactos','/economia','/notificaciones','/notarias','/visitas','/expedientes/nuevo']);
 
 export default function OperationalShellGate(){
  const {pathname}=useLocation();
