06f564e99cfa94d012e214831a844868a12fc5b8
carlosjmunoz78 <130542695+carlosjmunoz78@users.noreply.github.com>
2026-08-22 03:15:46 +0200

    fix(preprod): hard isolate dedicated operational screens

---
+++ src/OperationalShellGate.tsx
@@ -0,0 +1,10 @@
+import {useLocation} from 'react-router-dom';
+import OperationalShellV2 from './OperationalShellV2';
+
+const dedicatedExact=new Set(['/contactos','/inmobiliarias','/tasaciones','/agenda','/firmas','/documentacion','/financieros','/visitadores','/informes','/buscar','/bancos','/bancos/contactos']);
+
+export default function OperationalShellGate(){
+ const {pathname}=useLocation();
+ if(dedicatedExact.has(pathname)||/^\/bancos\/contactos\//.test(pathname)||/^\/bancos\/[^/]+$/.test(pathname)||/^\/financieros\/[^/]+$/.test(pathname)||/^\/visitadores\/[^/]+$/.test(pathname))return null;
+ return <OperationalShellV2/>;
+}
