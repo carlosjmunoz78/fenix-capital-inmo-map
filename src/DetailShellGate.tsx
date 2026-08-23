f2266f1b21d05cba60765860dde38a4440dd474f
carlosjmunoz78 <130542695+carlosjmunoz78@users.noreply.github.com>
2026-08-23 14:44:23 +0200

    feat(preprod): mount authorized nav on expediente detail

--- src/DetailShellGate.tsx
+++ src/DetailShellGate.tsx
@@ -1,8 +1,9 @@
 import {useLocation} from 'react-router-dom';
 import DetailShell from './DetailShell';
+import ExpedienteDetailAuthorizedNav from './ExpedienteDetailAuthorizedNav';
 
 export default function DetailShellGate(){
  const {pathname}=useLocation();
  if(pathname==='/expedientes/nuevo'||/^\/contactos\/[^/]+$/.test(pathname)||/^\/inmobiliarias\/[^/]+$/.test(pathname))return null;
- return <DetailShell/>;
+ return <><DetailShell/><ExpedienteDetailAuthorizedNav/></>;
 }
