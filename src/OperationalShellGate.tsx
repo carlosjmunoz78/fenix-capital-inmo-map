ddd7d4de612ee20b492617c028cde24b7b6fad05
carlosjmunoz78 <130542695+carlosjmunoz78@users.noreply.github.com>
2026-08-27 10:14:07 +0200

    refactor(preprod): route expedientes through shared shell

--- src/OperationalShellGate.tsx
+++ src/OperationalShellGate.tsx
@@ -1,10 +1,12 @@
 import {useLocation} from 'react-router-dom';
 import OperationalShellV2 from './OperationalShellV2';
+import ExpedientesSharedShell from './ExpedientesSharedShell';
 
 const dedicatedExact=new Set(['/contactos','/inmobiliarias','/tasaciones','/agenda','/firmas','/documentacion','/financieros','/visitadores','/informes','/buscar','/bancos','/bancos/contactos','/economia','/notificaciones','/notarias','/visitas','/expedientes/nuevo']);
 
 export default function OperationalShellGate(){
  const {pathname}=useLocation();
+ if(pathname==='/expedientes')return <ExpedientesSharedShell/>;
  if(
   dedicatedExact.has(pathname)
   || /^\/expedientes\/[^/]+$/.test(pathname)
