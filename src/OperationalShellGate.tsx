c7b3962d4b3240b4d3c231c1b210ed7f15f67f52
carlosjmunoz78 <130542695+carlosjmunoz78@users.noreply.github.com>
2026-08-31 21:06:40 +0200

    fix(ui): isolate dedicated create routes

--- src/OperationalShellGate.tsx
+++ src/OperationalShellGate.tsx
@@ -3,15 +3,23 @@ import OperationalShellV2 from './OperationalShellV2';
 import ExpedientesSharedShell from './ExpedientesSharedShell';
 import FirmaCreateShell from './FirmaCreateShell';
 
-const dedicatedExact=new Set(['/contactos','/inmobiliarias','/tasaciones','/agenda','/firmas','/documentacion','/financieros','/visitadores','/informes','/buscar','/bancos','/bancos/contactos','/economia','/notificaciones','/notarias','/visitas','/expedientes/nuevo']);
+const dedicatedExact=new Set([
+ '/contactos','/contactos/nuevo',
+ '/inmobiliarias','/inmobiliarias/nueva',
+ '/tasaciones','/agenda','/firmas','/documentacion','/financieros','/visitadores','/informes','/buscar',
+ '/bancos','/bancos/nuevo','/bancos/contactos',
+ '/economia','/notificaciones','/notarias','/visitas','/expedientes/nuevo'
+]);
 
 export default function OperationalShellGate(){
  const {pathname}=useLocation();
  if(pathname==='/expedientes')return <ExpedientesSharedShell/>;
- if(pathname==='/firmas/nuevo')return <FirmaCreateShell/>;
+ if(pathname==='/firmas/nuevo'||pathname==='/firmas/nueva')return <FirmaCreateShell/>;
  if(
   dedicatedExact.has(pathname)
   || /^\/expedientes\/[^/]+$/.test(pathname)
+  || /^\/contactos\/[^/]+$/.test(pathname)
+  || /^\/inmobiliarias\/[^/]+$/.test(pathname)
   || /^\/tareas\/[^/]+$/.test(pathname)
   || /^\/bancos\/contactos\//.test(pathname)
   || /^\/bancos\/[^/]+$/.test(pathname)
