b5bdafe1f24648f46fd1edf9cee3189fcb646738
carlosjmunoz78 <130542695+carlosjmunoz78@users.noreply.github.com>
2026-09-02 01:39:34 +0200

    fix(expedientes): remove legacy lifecycle DOM mutations

--- src/DetailShellGate.tsx
+++ src/DetailShellGate.tsx
@@ -1,10 +1,9 @@
 import {useLocation} from 'react-router-dom';
 import DetailShell from './DetailShell';
-import ExpedienteLifecycleGuard from './ExpedienteLifecycleGuard';
 
 export default function DetailShellGate(){
  const {pathname}=useLocation();
  const isExpedienteDetail=/^\/expedientes\/[^/]+$/.test(pathname)&&pathname!=='/expedientes/nuevo';
  if(!isExpedienteDetail)return null;
- return <><DetailShell/><ExpedienteLifecycleGuard/></>;
+ return <DetailShell/>;
 }
