2ba5bde71639f7a291f4182482c30ff6b7abcd84
carlosjmunoz78 <130542695+carlosjmunoz78@users.noreply.github.com>
2026-09-04 00:28:15 +0200

    Mount recovery placement fix with document intelligence

--- src/IntelligentDocumentIngestionGuard.tsx
+++ src/IntelligentDocumentIngestionGuard.tsx
@@ -1,11 +1,13 @@
 import UniversalDocumentIntelligenceGuardV2 from './UniversalDocumentIntelligenceGuardV2';
 import AudioTranscriptionGuard from './AudioTranscriptionGuard';
 import HistoricalDocumentRecoveryGuard from './HistoricalDocumentRecoveryGuard';
+import HistoricalDocumentRecoveryPlacementFix from './HistoricalDocumentRecoveryPlacementFix';
 
 export default function IntelligentDocumentIngestionGuard(){
   return <>
     <UniversalDocumentIntelligenceGuardV2 />
     <AudioTranscriptionGuard />
     <HistoricalDocumentRecoveryGuard />
+    <HistoricalDocumentRecoveryPlacementFix />
   </>;
 }
