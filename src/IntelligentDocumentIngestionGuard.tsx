2dd4ff152f4953f2fa07a49f83308a0c065f3263
carlosjmunoz78 <130542695+carlosjmunoz78@users.noreply.github.com>
2026-09-03 20:47:07 +0200

    Mount L5 dictation beside document intelligence

--- src/IntelligentDocumentIngestionGuard.tsx
+++ src/IntelligentDocumentIngestionGuard.tsx
@@ -1,1 +1,9 @@
-export {default} from './UniversalDocumentIntelligenceGuardV2';
+import UniversalDocumentIntelligenceGuardV2 from './UniversalDocumentIntelligenceGuardV2';
+import AudioTranscriptionGuard from './AudioTranscriptionGuard';
+
+export default function IntelligentDocumentIngestionGuard(){
+  return <>
+    <UniversalDocumentIntelligenceGuardV2 />
+    <AudioTranscriptionGuard />
+  </>;
+}
