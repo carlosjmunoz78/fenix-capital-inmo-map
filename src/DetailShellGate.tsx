f525a57e1dbd3b4a5e85bb40ce9a45852bfe2a53
carlosjmunoz78 <130542695+carlosjmunoz78@users.noreply.github.com>
2026-09-07 09:02:10 +0200

    feat: mount certified expediente rename in detail

--- src/DetailShellGate.tsx
+++ src/DetailShellGate.tsx
@@ -2,10 +2,11 @@ import {useLocation} from 'react-router-dom';
 import DetailShell from './DetailShell';
 import ExpedienteLifecycleGuard from './ExpedienteLifecycleGuard';
 import ExpedienteManualPhaseGuard from './ExpedienteManualPhaseGuard';
+import ExpedienteRenameGuard from './ExpedienteRenameGuard';
 
 export default function DetailShellGate(){
  const {pathname}=useLocation();
  const isExpedienteDetail=/^\/expedientes\/[^/]+$/.test(pathname)&&pathname!=='/expedientes/nuevo';
  if(!isExpedienteDetail)return null;
- return <><DetailShell/><ExpedienteLifecycleGuard/><ExpedienteManualPhaseGuard/></>;
+ return <><DetailShell/><ExpedienteRenameGuard/><ExpedienteLifecycleGuard/><ExpedienteManualPhaseGuard/></>;
 }
