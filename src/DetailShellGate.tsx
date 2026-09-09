e8abd0ef10fd96a7aa6ea969e8cdcfc67a92be41
carlosjmunoz78 <130542695+carlosjmunoz78@users.noreply.github.com>
2026-09-09 09:45:10 +0200

    fix: mount expediente rename guard only once

--- src/DetailShellGate.tsx
+++ src/DetailShellGate.tsx
@@ -1,11 +1,10 @@
 import {useLocation} from 'react-router-dom';
 import DetailShell from './DetailShell';
 import ExpedienteLifecycleGuard from './ExpedienteLifecycleGuard';
-import ExpedienteRenameGuard from './ExpedienteRenameGuard';
 
 export default function DetailShellGate(){
  const {pathname}=useLocation();
  const isExpedienteDetail=/^\/expedientes\/[^/]+$/.test(pathname)&&pathname!=='/expedientes/nuevo';
  if(!isExpedienteDetail)return null;
- return <><DetailShell/><ExpedienteRenameGuard/><ExpedienteLifecycleGuard/></>;
+ return <><DetailShell/><ExpedienteLifecycleGuard/></>;
 }
