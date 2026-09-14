0a245bfb36af1dcb72d05bf744b53d48cf1d3f2b
carlosjmunoz78 <130542695+carlosjmunoz78@users.noreply.github.com>
2026-09-14 13:32:36 +0200

    fix: mount expediente alias and participant restoration

--- src/DetailShellGate.tsx
+++ src/DetailShellGate.tsx
@@ -4,10 +4,11 @@ import ExpedienteLifecycleGuard from './ExpedienteLifecycleGuard';
 import ExpedienteManualPhaseGuard from './ExpedienteManualPhaseGuard';
 import ExpedienteRenameGuard from './ExpedienteRenameGuard';
 import ExpedientePeopleProdGuard from './ExpedientePeopleProdGuard';
+import ExpedienteAliasDisplayGuard from './ExpedienteAliasDisplayGuard';
 
 export default function DetailShellGate(){
  const {pathname}=useLocation();
  const isExpedienteDetail=/^\/expedientes\/[^/]+$/.test(pathname)&&pathname!=='/expedientes/nuevo';
  if(!isExpedienteDetail)return null;
- return <><DetailShell/><ExpedienteRenameGuard/><ExpedientePeopleProdGuard/><ExpedienteLifecycleGuard/><ExpedienteManualPhaseGuard/></>;
+ return <><DetailShell/><ExpedienteAliasDisplayGuard/><ExpedienteRenameGuard/><ExpedientePeopleProdGuard/><ExpedienteLifecycleGuard/><ExpedienteManualPhaseGuard/></>;
 }
